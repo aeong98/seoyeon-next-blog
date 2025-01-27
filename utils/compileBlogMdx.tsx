dedabee2b6185528c19a58742f6b634f5364cebb
seoyeonkim01 <seoyeonkim@creatrip.com>
2025-01-27 21:43:46 +0900

    feat: code syntax highlighting 기능 추가

--- utils/compileBlogMdx.tsx
+++ utils/compileBlogMdx.tsx
@@ -1,5 +1,7 @@
 import { compileMDX } from "next-mdx-remote/rsc";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 async function compileBlogMdx(fileContent: string) {
   const { content, frontmatter } = await compileMDX<{
@@ -9,7 +11,21 @@ async function compileBlogMdx(fileContent: string) {
   }>({
     source: fileContent,
     options: { parseFrontmatter: true },
-    components: { Image: (props) => <Image {...props} /> },
+    components: {
+      Image: (props) => <Image {...props} />,
+      code: ({ children, className }) => {
+        const language = className?.replace("language-", "");
+        if (!language) {
+          return <span className="font-bold">{children}</span>;
+        }
+
+        return (
+          <SyntaxHighlighter style={materialDark} language={language}>
+            {String(children).replace(/\n$/, "")}
+          </SyntaxHighlighter>
+        );
+      },
+    },
   });
 
   const { title, description, date } = frontmatter;
